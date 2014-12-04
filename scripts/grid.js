7949dcfbb05ceafd3faaa8d3de275afba63044b8
cairns <lazycairx@gmail.com>
2014-12-04 14:38:11 +0100

    Better calculation of the Grid system.

--- scripts/grid.js
+++ scripts/grid.js
@@ -5,7 +5,7 @@
 
 var Grid = (function() {
     var line = 1;
-    var space = 20;
+    var space = 19;
 
     var width = 961;
     var height = 801;
@@ -15,11 +15,11 @@ var Grid = (function() {
         context.lineWidth = line;
         context.beginPath();
 
-        for (var x = 0; x < width; x += space) {
+        for (var x = 0; x < width; x += space + line) {
             context.moveTo(add(x, 0.5), 0);
             context.lineTo(add(x, 0.5), height);
         }
-        for (var y = 0; y < height; y += space) {
+        for (var y = 0; y < height; y += space + line) {
             context.moveTo(0, add(y, 0.5));
             context.lineTo(width, add(y, 0.5));
         }
@@ -27,12 +27,16 @@ var Grid = (function() {
         context.stroke();
     };
 
+    var position = function(number) {
+        return add(mult(number, space), mult(number,line));
+    };
+
     var end = function(number) {
-        return sub(mult(number, space), line);
+        return sub(position(number), line);
     };
 
     var start = function(number) {
-        return add(mult(number, space), line);
+        return add(position(number), line);
     };
 
     var snap = function(number) {
